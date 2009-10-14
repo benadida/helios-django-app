10a8fbfeec0a40a1e5eb65381d8263593b89a324
Ben Adida <ben@adida.net>
2009-10-14 12:45:46 -0700

    upped limit

--- views.py
+++ views.py
@@ -326,7 +326,7 @@ def one_election_bboard(request, election):
   UI to show election bboard
   """
   after = request.GET.get('after', None)
-  limit = int(request.GET.get('limit', 50))
+  limit = int(request.GET.get('limit', 1000))
 
   # if there's a specific voter
   if request.GET.has_key('q'):
